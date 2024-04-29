132bfb940f96bbb2586bb1131ac3f1d2b667c65c
nashiruddin47 <nashiruddin139@gmail.com>
2024-04-29 17:43:47 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+// Home PAGES
+
+import Card from "./components/card/Card";
+
+const Home  = ()  => {
+    return (
+        <div>
+            <h1>React Project</h1>
+            <Card/>
+
+            
+        </div>
+    )
+}
+
+
+export default Home;
---
+++ src/components/card/Card.tsx
@@ -0,0 +1,15 @@
+
+const Card = ()  => {
+    return (
+        <div className="flex">
+            <div className="itm">
+                <h3>hre</h3>
+            </div>
+            <div className="itm"></div>
+            <div className="itm"></div>
+
+        </div>
+    )
+}
+
+export default Card;
